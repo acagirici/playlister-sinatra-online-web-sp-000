10b4c54117f5da56b483c4c0dfc2bde602fb0179
Arif Cagirici <acagirici23@gmail.com>
2021-01-09 01:53:16 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,13 +1,9 @@
+require_relative "./concerns/slugifiable"
 class Genre < ActiveRecord::Base
-   has_many :song_genres
-   has_many :songs, through: :song_genres
-   has_many :artists, through: :songs
+	has_many :song_genres
+	has_many :songs, through: :song_genres
+	has_many :artists, through: :songs
 
-   def slug
-      self.name.downcase.gsub(" ", "-")
-    end
-   
-   def self.find_by_slug(slug)
-      self.all.find{|artist| artist.slug == slug}
-    end
+	include Slugifiable::InstanceMethods
+	extend Slugifiable::ClassMethods
 end
