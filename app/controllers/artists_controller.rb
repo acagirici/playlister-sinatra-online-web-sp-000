e56243b102c57c6f5f4b2a99489a6e2f6465ddf7
Arif Cagirici <acagirici23@gmail.com>
2021-01-09 01:37:45 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,7 @@
-require 'rack-flash'
-
-class ArtistsController < ApplicationController
-  use Rack::Flash
+class ArtistsController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
   get '/artists' do
       @artists = Artist.all
       erb :'/artists/index'
